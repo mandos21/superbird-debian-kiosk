601937564d48fcf8290e40d184e987d891c36a04
Matthew DeGenaro <mt.degenaro@gmail.com>
2025-01-04 15:05:08 -0500

    Added media_player knob control support

--- files/data/scripts/buttons_app.py
+++ files/data/scripts/buttons_app.py
@@ -14,6 +14,8 @@ import struct
 import contextlib
 import warnings
 import logging
+import queue
+import threading
 
 from threading import Thread
 from threading import Event as ThreadEvent
@@ -25,7 +27,7 @@ from urllib3.exceptions import InsecureRequestWarning
 from homeassistant_api import Client
 
 # user-configurable settings are all in button_settings.py
-from buttons_settings import ROOM_LIGHT, ROOM_SCENES, ESC_SCENE, LEVEL_INCREMENT
+from buttons_settings import ROOM_LIGHT, ROOM_SCENES, ESC_SCENE, LEVEL_INCREMENT, MEDIA_PLAYER
 from buttons_settings import HA_SERVER, HA_TOKEN
 
 # All the device buttons are part of event0, which appears as a keyboard
@@ -37,6 +39,8 @@ from buttons_settings import HA_SERVER, HA_TOKEN
 
 DEV_BUTTONS = '/dev/input/event0'
 DEV_KNOB = '/dev/input/event1'
+
+KNOB_MODE = MEDIA_PLAYER or ROOM_LIGHT
 
 # for event0, these are the keycodes for buttons
 BUTTONS_CODE_MAP = {
@@ -59,7 +63,7 @@ EVENT_FORMAT = 'llHHI'
 EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 
 # global for HA Client
-HA_CLIENT:Client = None
+HA_CLIENT: Client = None
 
 # suppress warnings about invalid certs
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -146,14 +150,17 @@ def handle_button(pressed_key: str):
             preset = ROOM_SCENES[int(pressed_key) - 1]
             cmd_scene(preset)
     elif pressed_key in ['ESC', 'ENTER', 'LEFT', 'RIGHT']:
-        if pressed_key == 'ENTER':
-            cmd_toggle()
-        elif pressed_key == 'LEFT':
-            cmd_lower()
-        elif pressed_key == 'RIGHT':
-            cmd_raise()
         if pressed_key == 'ESC':
             cmd_scene(ESC_SCENE)
+        if pressed_key == 'ENTER':
+            if KNOB_MODE == MEDIA_PLAYER:
+                cmd_playback_toggle()
+            else:
+                cmd_light_toggle()
+        elif pressed_key == 'LEFT':
+            cmd_light_lower()
+        elif pressed_key == 'RIGHT':
+            cmd_light_raise()
 
 
 def get_light_level(entity_id: str) -> int:
@@ -188,7 +195,7 @@ def cmd_scene(entity_id: str):
     scene_domain.turn_on(entity_id=entity_id)
 
 
-def cmd_toggle():
+def cmd_light_toggle():
     """
     Toggle the light for this room on/off
     """
@@ -197,7 +204,7 @@ def cmd_toggle():
     light_domain.toggle(entity_id=ROOM_LIGHT)
 
 
-def cmd_lower():
+def cmd_light_lower():
     """
     Lower the level of the light for this room
     """
@@ -210,7 +217,7 @@ def cmd_lower():
         set_light_level(ROOM_LIGHT, new_level)
 
 
-def cmd_raise():
+def cmd_light_raise():
     """
     Raise the level of the light for this room
     """
@@ -223,14 +230,51 @@ def cmd_raise():
         set_light_level(ROOM_LIGHT, new_level)
 
 
-class EventListener():
+def cmd_playback_toggle():
     """
-    Listen to a specific /dev/eventX and call handle_button 
+    Toggle the playback for this device play/pause
     """
+    logger.info(f'Toggling state of media_player: {MEDIA_PLAYER}')
+    media_player_domain = HA_CLIENT.get_domain('media_player')
+    media_player_domain.media_play_pause(entity_id=MEDIA_PLAYER)
+
+
+def get_volume_level() -> int:
+    """
+    Get current volume level of media player
+    """
+    logger.info(f'Getting volume of media_player: {MEDIA_PLAYER}')
+    media_player = HA_CLIENT.get_entity(entity_id=MEDIA_PLAYER)
+    level = media_player.get_state().attributes['volume_level']
+    logger.info(f'media_player current volume: {level}')
+    if level is None:
+        level = 0
+    return level
+
+
+def set_volume_level(level: float):
+    """
+    Set current volume level of media player
+    """
+    logger.info(f'Setting volume of media_player: {MEDIA_PLAYER}')
+    media_player_domain = HA_CLIENT.get_domain('media_player')
+    media_player_domain.volume_set(entity_id=MEDIA_PLAYER, volume_level=level)
+
+
+class EventListener:
+    """
+    Listen to a specific /dev/eventX and call handle_button.
+    Improved to batch LEFT/RIGHT events for volume control.
+    """
+
     def __init__(self, device: str) -> None:
         self.device = device
         self.stopper = ThreadEvent()
-        self.thread:Thread = None
+        self.thread: Thread = None
+        self.event_queue = queue.Queue()  # Thread-safe queue for knob events
+        self.volume_timer = None  # Timer for batching volume events
+        self.volume_delta = 0  # Cumulative delta for volume changes
+        self.volume_lock = threading.Lock()  # Lock for accessing volume delta
         self.start()
 
     def start(self):
@@ -251,7 +295,7 @@ class EventListener():
 
     def listen(self):
         """
-        To run in thread, listen for events and call handle_buttons if applicable
+        To run in thread, listen for events and call handle_buttons if applicable.
         """
         with open(self.device, "rb") as in_file:
             event = in_file.read(EVENT_SIZE)
@@ -259,11 +303,46 @@ class EventListener():
                 if self.stopper.is_set():
                     break
                 (_sec, _usec, etype, code, value) = struct.unpack(EVENT_FORMAT, event)
-                # logger.info(f'Event: type: {etype}, code: {code}, value:{value}')
                 event_str = translate_event(etype, code, value)
-                if event_str in ['1', '2', '3', '4', 'm', 'ENTER', 'ESC', 'LEFT', 'RIGHT']:
+                if event_str in ['LEFT', 'RIGHT'] and KNOB_MODE == MEDIA_PLAYER:
+                    self.handle_knob(event_str)
+                elif event_str in ['1', '2', '3', '4', 'm', 'ENTER', 'ESC', 'LEFT', 'RIGHT']:
                     handle_button(event_str)
                 event = in_file.read(EVENT_SIZE)
+
+    def handle_knob(self, direction: str):
+        """
+        Handle LEFT/RIGHT knob events with batching.
+        """
+        with self.volume_lock:
+            # Adjust volume delta based on direction
+            if direction == 'LEFT':
+                self.volume_delta -= 0.025
+            elif direction == 'RIGHT':
+                self.volume_delta += 0.025
+
+        # Start/reset the batching timer
+        if not self.volume_timer:
+            self.volume_timer = threading.Timer(0.2, self.process_volume_delta)
+            self.volume_timer.start()
+
+    def process_volume_delta(self):
+        """
+        Process and apply the batched volume changes.
+        """
+        with self.volume_lock:
+            delta = self.volume_delta
+            self.volume_delta = 0  # Reset delta
+
+        # Apply the cumulative volume change
+        logger.info(f'Applying cumulative volume delta: {delta}')
+        current_level = get_volume_level()
+        new_level = current_level + delta
+        new_level = max(0, min(1, new_level))  # Clamp between 0 and 1
+        set_volume_level(new_level)
+
+        # Reset the timer
+        self.volume_timer = None
 
 
 if __name__ == '__main__':
--- files/data/scripts/buttons_settings.py
+++ files/data/scripts/buttons_settings.py
@@ -9,12 +9,16 @@ HA_SERVER = 'https://192.168.1.144:8123'
 # long-lived token, https://www.home-assistant.io/docs/authentication/#your-account-profile
 HA_TOKEN = 'insert-token-here'
 
-# Light entity to control with knob
-ROOM_LIGHT = 'light.office'
+# Light entity to control with knob or media_player
+# Leave one empty
+ROOM_LIGHT = ''
+MEDIA_PLAYER = 'media_player.office_speaker'
 
 # when you turn the knob, brightness will go up or down by this amount
 #   brightness is 0 - 255
-LEVEL_INCREMENT = 32
+# LEVEL_INCREMENT = 32
+# for volume adjustment uncomment the following line and comment out the above
+LEVEL_INCREMENT = .025
 
 # assign scene/automation/script to buttons along the edge
 #   anything that supports turn_on() should work
